cacafef6eb7d7ccbdf23d6c7f7675575f2648c61
unknown <keegood8@gmail.com>
2021-04-10 19:24:00 -0700

    registered models, added models & urls

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+class User(AbstractUser):
+    email = models.EmailField(
+        'email address',
+        unique=True,
+        error_messages={ # overwrite default error message for unique constraint
+            'unique':"This email has already been registered."
+        }
+    )
+
+    def __str__(self):
+        return self.username
+
+class RefreshToken(models.Model):
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    token = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f"{self.user}'s refresh token"
