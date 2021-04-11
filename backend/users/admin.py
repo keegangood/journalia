f17a8347c06d045c5ab1ac83761ac951f5d657be
unknown <keegood8@gmail.com>
2021-04-10 20:05:37 -0700

    customize UserAdmin, registered user models

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,6 +1,27 @@
 from django.contrib import admin
-
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
+from django.utils.translation import ugettext_lazy as _
 from .models import User, RefreshToken
 
-# register models in admin
-admin.site.register([User, RefreshToken])
+@admin.register(User)
+class UserAdmin(DjangoUserAdmin):
+    """Define admin model for custom User model with no email field."""
+
+    fieldsets = (
+        (None, {'fields': ('email', 'username', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
+                                       'groups', 'user_permissions')}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'username', 'password1', 'password2'),
+        }),
+    )
+    list_display = ('email', 'username', 'first_name', 'last_name', 'is_staff')
+    search_fields = ('email', 'username', 'first_name', 'last_name')
+    ordering = ('email',)
+
+admin.site.register(RefreshToken)
