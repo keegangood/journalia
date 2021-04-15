f79573a5038493270db18201507e1f6335eb9c57
unknown <keegood8@gmail.com>
2021-04-15 15:40:06 -0700

    added Note model and registered

--- backend/main/settings.py
+++ backend/main/settings.py
@@ -33,6 +33,7 @@ INSTALLED_APPS = [
     'corsheaders',
 
     'users',
+    'notes',
 
 ]
 
--- backend/notes/models.py
+++ backend/notes/models.py
@@ -5,11 +5,9 @@ from django.utils.translation import ugettext_lazy as _
 
 class Note(models.Model):
     title = models.CharField(_('title'), max_length=10000)
-    owner = models.ForeignKey(get_user_model(), name=_(
-        'owner'), on_delete=models.CASCADE)
+    owner = models.ForeignKey(get_user_model(), verbose_name=_('owner'), on_delete=models.CASCADE)
     is_important = models.BooleanField(_('important'), default=False)
     is_research = models.BooleanField(_('research'), default=False)
     is_good_idea = models.BooleanField(_('good idea'), default=False)
     date_created = models.DateTimeField(_('date created'), auto_now_add=True)
     last_modified = models.DateTimeField(_('date edited'), blank=True, null=True)
-    
