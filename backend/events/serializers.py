e550f547f32f3e7e5add9f52505993c6cf269d9c
keegangood <keegood8@gmail.com>
2021-05-20 19:47:24 -0700

    added EventCreateSerializer

--- backend/events/serializers.py
+++ backend/events/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from rest_framework.views import set_rollback
 from .models import Event
 
 
@@ -6,6 +7,12 @@ class EventDetailSerializer(serializers.ModelSerializer):
 
     item_type = serializers.CharField(max_length=1, default = 'E') # 'E' for 'Event'
 
+    class Meta:
+        model = Event
+        fields = '__all__'
+
+
+class EventCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Event
         fields = '__all__'
