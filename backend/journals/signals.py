01318b370f5ee49a5beb196401cd17712a6883c5
keegangood <keegood8@gmail.com>
2021-05-20 19:44:54 -0700

    added pre_delete signal for JournalItems

---
+++ backend/journals/signals.py
@@ -0,0 +1,19 @@
+from django.db.models.signals import pre_delete, pre_save
+from django.dispatch import receiver
+from notes.models import Note
+from tasks.models import Task
+from events.models import Event
+
+from .models import JournalItem
+
+@receiver(pre_delete, sender=JournalItem)
+def delete_content_object(sender, instance, **kwargs):
+    '''
+    Before delteting a JournalItem:
+    - delete all of its children and their content_objects 
+    - delete its content_object
+    '''
+    
+    [child.delete() for child in instance.children.all()]
+
+    instance.content_object.delete()
