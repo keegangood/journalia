622a6015fc3055811fcbdbe3cb14a044dc04700d
keegangood <keegood8@gmail.com>
2021-05-20 19:45:51 -0700

    imported signals

--- backend/journals/apps.py
+++ backend/journals/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class JournalsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'journals'
+
+    def ready(self) -> None:
+        import journals.signals
