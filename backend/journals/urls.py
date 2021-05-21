b16f3353bc163dcef4bf621cac8747eebe3cd9d1
keegangood <keegood8@gmail.com>
2021-05-20 19:46:48 -0700

    added delete url

--- backend/journals/urls.py
+++ backend/journals/urls.py
@@ -5,5 +5,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.journal_item_list)
+    path('', views.journal_item_list),
+    path('delete/', views.delete_journal_item)
 ]
