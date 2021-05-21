2d44aec7e05d4fd19bd3270752ceebe4adf0f4c1
keegangood <keegood8@gmail.com>
2021-05-20 19:48:12 -0700

    removed old custom create method

--- backend/notes/serializers.py
+++ backend/notes/serializers.py
@@ -4,15 +4,9 @@ from .models import Note
 class NoteCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Note
-        exclude=['owner',]
-
+        fields = '__all__'
     # date_created = serializers.DateTimeField()
     # last_modified = serializers.DateTimeField()
-
-    def create(self, validated_data):
-
-        validated_data['owner'] = self.context['request'].user
-        return super().create(validated_data)
 
 
 class NoteDetailSerializer(serializers.ModelSerializer):
