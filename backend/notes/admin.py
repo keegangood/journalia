c23de5c37254365166f9547a251ddf7f9236b424
keegangood <keegood8@gmail.com>
2021-05-23 23:07:14 -0700

    removed NoteAdmin

--- backend/notes/admin.py
+++ backend/notes/admin.py
@@ -1,9 +1,4 @@
 from django.contrib import admin
 from .models import Note
 
-class NoteAdmin(admin.ModelAdmin):
-    model = Note
-
-    fields=('title',)
-    readonly_fields=('children',)
-admin.site.register(Note, NoteAdmin)
+admin.site.register(Note)
