bd9ce4e6bc17c7cbfca521a11840adba2e9e80a4
keegangood <keegood8@gmail.com>
2021-05-23 23:02:34 -0700

    added read_only_field for item_type

--- backend/events/serializers.py
+++ backend/events/serializers.py
@@ -4,12 +4,13 @@ from .models import Event
 
 
 class EventDetailSerializer(serializers.ModelSerializer):
-
-    item_type = serializers.CharField(max_length=1, default = 'E') # 'E' for 'Event'
-
     class Meta:
         model = Event
         fields = '__all__'
+        read_only_fields = ['item_type',]
+
+    item_type = serializers.CharField(max_length=1, default = 'E') # 'E' for 'Event'
+
 
 
 class EventCreateSerializer(serializers.ModelSerializer):
