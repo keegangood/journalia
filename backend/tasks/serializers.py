2bf9bbc62b7dc8f63ada9356696eade6e86bad56
keegangood <keegood8@gmail.com>
2021-05-23 23:09:02 -0700

    added read_only_field for item_type

--- backend/tasks/serializers.py
+++ backend/tasks/serializers.py
@@ -2,12 +2,12 @@ from rest_framework import serializers
 from .models import Task
 
 class TaskDetailSerializer(serializers.ModelSerializer):
-
-    item_type = serializers.CharField(max_length=1, default = 'T') # 'T' for Task
-
     class Meta:
         model = Task
         fields = '__all__'
+        read_only_fields = ['item_type',]
+
+    item_type = serializers.CharField(max_length=1, default = 'T') # 'T' for Task
 
 class TaskCreateSerializer(serializers.ModelSerializer):
     class Meta:
