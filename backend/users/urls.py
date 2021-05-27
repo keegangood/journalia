5487e2f4ca59655239f9d0266fb1ecaa40514aae
keegangood <keegood8@gmail.com>
2021-05-26 21:36:42 -0700

    added request methods in comments for each url

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -5,10 +5,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.register), # create
-    path('auth/', views.auth), # get logged in user
-    path('login/', views.login), # login user
-    path('token/', views.extend_token), # request new access tokens
-    path('<int:pk>/', views.user_detail), # read/update
-    path('logout/', views.logout), # delete tokens
+    path('', views.register), # POST - create
+    path('auth/', views.auth), # GET - get logged in user
+    path('login/', views.login), # POST - login user
+    path('token/', views.extend_token), # GET - request new access tokens
+    path('<int:pk>/', views.user_detail), # GET, PUT - read/update
+    path('logout/', views.logout), # POST - delete tokens
 ]
