c5cdea04a6b01e4499d1ef9884694671f8812112
keegangood <keegood8@gmail.com>
2021-06-06 17:35:29 -0700

    created homepage component

---
+++ journalia_frontend/src/components/pages/Homepage.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import '../../scss/homepage.scss';
+
+const Homepage = () => {
+  return (
+    <div className="container-fluid w-90" id="homepage">
+      <div className="boxes">
+        <div className="box" id='box1'></div>
+        <div className="box" id='box2'></div>
+        <div className="box" id='box3'></div>
+        <div className="box" id='box4'></div>
+        <div className="box" id='box5'></div>
+        <div className="box" id='box6'></div>
+        <div className="box" id='box7'></div>
+        <div className="box" id='box8'></div>
+        <div className="box" id='box9'></div>
+        <div className="box" id='box10'></div>
+        <div className="box" id='box11'></div>
+        <div className="box" id='box12'></div>
+        <div className="box" id='box13'></div>
+      </div>
+    </div>
+  )
+}
+
+export default Homepage
