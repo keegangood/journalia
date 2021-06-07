718e4b0cf81ed2e81b188f83b8c4f51ec665209d
keegangood <keegood8@gmail.com>
2021-06-06 17:35:58 -0700

    created desktop Navbar component

---
+++ journalia_frontend/src/components/layout/Navbar.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import '../../scss/navbar.scss';
+
+import { ReactComponent as Logo } from '../../assets/img/Logo.svg';
+
+const Navbar = () => {
+    return (
+        <nav class="navbar navbar-expand-lg navbar-dark py-0" id="desktop-nav">
+            <div className="container-fluid d-flex" id="navbar-contents">
+                <a href="#" className="navbar-brand">
+                    <Logo />
+                </a>
+
+                <ul className="navbar-nav">
+                    <li className="nav-item d-flex">
+                        <a href="#" className="nav-link">Sign up</a>
+                    </li>
+                    <li className="nav-item d-flex">
+                        <a href="#" className="nav-link">Log in</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar;
