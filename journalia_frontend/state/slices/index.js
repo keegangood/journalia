a47aa55b27da1eb9bc52354000f5f302392568d0
keegangood <keegood8@gmail.com>
2021-06-06 21:48:06 -0700

    setup auth state

---
+++ journalia_frontend/state/slices/index.js
@@ -0,0 +1,8 @@
+import {combineReducers} from '@reduxjs/toolkit';
+import UsersSlice from './crushCloneSlice';
+
+const rootReducer = combineReducers({
+  AuthSlice
+})
+
+export default rootReducer;
