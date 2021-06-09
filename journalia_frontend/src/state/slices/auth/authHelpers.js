34e10ab396a8000316f6f8e462549fd2454f70e9
keegangood <keegood8@gmail.com>
2021-06-08 20:27:16 -0700

    stubbed out auth helper functions

--- journalia_frontend/src/state/slices/auth/authHelpers.js
+++ journalia_frontend/src/state/slices/auth/authHelpers.js
@@ -1,9 +1,9 @@
 import 'axios';
 
-export const requestAccessToken = async () => {};
-export const register = async () => {};
-export const login = async () => {};
-export const loadUser = async () => {};
-export const logout = async () => {};
+export const requestAccessToken = async (url) => {};
+export const register = async (url) => {};
+export const login = async (url) => {};
+export const loadUser = async (url) => {};
+export const logout = async (url) => {};
 
 
