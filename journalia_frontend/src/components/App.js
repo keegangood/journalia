230c75c76ad5b04b512bb5f0c1b996bfbb566bcb
keegangood <keegood8@gmail.com>
2021-06-08 20:34:12 -0700

    added Router and browser history

--- journalia_frontend/src/components/App.js
+++ journalia_frontend/src/components/App.js
@@ -1,16 +1,30 @@
 import './App.scss';
+import { BrowserRouter as Router, Route, useLocation } from 'react-router-dom';
 
+import { createBrowserHistory } from 'history';
 
 import Navbar from '../components/layout/Navbar';
 import MobileNav from '../components/layout/MobileNav';
 
-import Homepage from '../components/pages/Homepage';
+import Homepage from '../components/pages/Homepage/Homepage';
+import Login from './pages/Login/Login';
+
+const history = createBrowserHistory();
 
 function App() {
+
+
   return (
-    <div className="container-fluid p-0" id="main-container">
-        <Navbar/>
-        <Homepage/>
+
+    <div className="app container-fluid p-0" id="main-container">
+      <Router>
+        {/* show nav unless on login or signup pages.
+        Eventually this will be based on isAuthenticated */}
+        {history.location.pathname !== '/login' && history.location.pathname !== '/signup' && <Navbar />}
+        <Route exact path="/" component={Homepage} />
+        <Route exact path="/login" component={Login} />
+        {/* <Homepage /> */}
+      </Router>
     </div>
   );
 }
