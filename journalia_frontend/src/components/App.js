efb38b567c8728e59385d86bcfe99bd8b62b7b47
keegangood <keegood8@gmail.com>
2021-06-13 22:53:56 -0700

    hooked up store

--- journalia_frontend/src/components/App.js
+++ journalia_frontend/src/components/App.js
@@ -1,35 +1,42 @@
-import './App.scss';
-import { BrowserRouter as Router, Route, useLocation } from 'react-router-dom';
+import "./App.scss";
+import { BrowserRouter as Router, Route, useLocation } from "react-router-dom";
 
-import { createBrowserHistory } from 'history';
+import { configureStore, applyMiddleware } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+import rootReducer from "../state/slices";
+import thunk from "redux-thunk";
 
-import Navbar from '../components/layout/Navbar';
-import MobileNav from '../components/layout/MobileNav';
+import { createBrowserHistory } from "history";
 
-import Homepage from '../components/pages/Homepage/Homepage';
-import UserAuth from './pages/UserAuth/UserAuth';
+import Navbar from "../components/layout/Navbar";
+import MobileNav from "../components/layout/MobileNav";
+
+import Homepage from "../components/pages/Homepage/Homepage";
+import UserAuth from "./pages/UserAuth/UserAuth";
 
 const history = createBrowserHistory();
 
+let store = configureStore({ reducer: rootReducer }, applyMiddleware(thunk));
+
 function App() {
-
-
   return (
-
-    <div className="app container-fluid p-0" id="main-container">
-      <Router>
-        {/* show nav unless on login or signup pages.
+    <Provider store={store}>
+      <div className="app container-fluid p-0 flex" id="main-container">
+        <Router>
+          {/* show nav unless on login or signup pages.
         Eventually this will be based on isAuthenticated */}
-        {history.location.pathname !== '/login' && history.location.pathname !== '/signup' && <Navbar />}
-        <Route exact path="/" component={Homepage} />
-        <Route exact path="/login">
-          <UserAuth pageAction={'login'} pageTitle={'Log in'}/>
-        </Route>
-        <Route exact path="/signup">
-          <UserAuth pageAction={'signup'} pageTitle={'Sign up'}/>
-        </Route>
-      </Router>
-    </div>
+          {history.location.pathname !== "/login" &&
+            history.location.pathname !== "/signup" && <Navbar />}
+          <Route exact path="/" component={Homepage} />
+          <Route exact path="/login">
+            <UserAuth pageAction={"login"} pageTitle={"Log in"} />
+          </Route>
+          <Route exact path="/signup">
+            <UserAuth pageAction={"signup"} pageTitle={"Sign up"} />
+          </Route>
+        </Router>
+      </div>
+    </Provider>
   );
 }
 
