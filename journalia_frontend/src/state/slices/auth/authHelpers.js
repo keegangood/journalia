2f69f1268f151a6de7c9340829066f57f0df7d07
keegangood <keegood8@gmail.com>
2021-06-13 22:54:40 -0700

    deleted unneccessary files

--- journalia_frontend/src/state/slices/auth/authHelpers.js
+++
@@ -1,9 +0,0 @@
-import 'axios';
-
-export const requestAccessToken = async (url) => {};
-export const register = async (url) => {};
-export const login = async (url) => {};
-export const loadUser = async (url) => {};
-export const logout = async (url) => {};
-
-
