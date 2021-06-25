24899853c7eb9299a472db89733a3bcf2852f6ab
unknown <keegood8@gmail.com>
2021-06-24 23:08:20 -0700

    cleaned up component, removed reference to auth state & replaced with props

--- journalia_frontend/src/components/pages/UserAuth/PrivateRoute.js
+++ journalia_frontend/src/components/pages/UserAuth/PrivateRoute.js
@@ -1,23 +1,21 @@
-import React, { useContext } from 'react';
-import { Route, Redirect } from 'react-router-dom';
+import { React } from "react";
+import { Route, Redirect } from "react-router-dom";
 
-import AuthContext from '../../context/auth/authContext';
-
-const PrivateRoute = ({ component: Component, ...rest }) => {
-  const authContext = useContext(AuthContext);
-
-  const { isAuthenticated, loading } = authContext;
-
+const PrivateRoute = ({
+  component: Component,
+  isAuthenticated,
+  authStatus,
+  ...rest
+}) => {
   return (
     <Route
       // pass the rest of the props
       {...rest}
-      render={props =>
+      render={(props) =>
         // if not authenticated when loaded, redirect to login page
-        !isAuthenticated && !loading ? (
-          <Redirect to='/login' />
+        !isAuthenticated && authStatus === "IDLE" ? (
+          <Redirect to="/login" />
         ) : (
-          // if authenticated, load the protected component
           <Component {...props} />
         )
       }
