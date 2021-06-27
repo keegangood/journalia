3e4c9ac61e73c36cd3e24f8209d36d7f58299015
unknown <keegood8@gmail.com>
2021-06-26 21:56:07 -0700

    added view elements and side menu

--- journalia_frontend/src/components/pages/Calendar/Calendar.js
+++ journalia_frontend/src/components/pages/Calendar/Calendar.js
@@ -1,26 +1,47 @@
-import {React, useEffect} from 'react'
-
+import { React, useEffect } from "react";
 import { useDispatch, useSelector, connect } from "react-redux";
+import { Router, Route, useHistory } from "react-router-dom";
 
-import {requestAccessToken} from '../../../state/slices/auth/AuthSlice';
+import "./scss/Calendar.scss";
 
-const Calendar = () => {
+import SideMenu from "./SideMenu";
+
+import DayView from "./DayView/DayView";
+import WeekView from "./WeekView/WeekView";
+import MonthView from "./MonthView/MonthView";
+import YearView from "./YearView/YearView";
+
+import { requestAccessToken } from "../../../state/slices/auth/AuthSlice";
+
+const Calendar = ({ history }) => {
   const dispatch = useDispatch();
 
   return (
-    <div>
-      CALENDAR
+    <div className="container-fluid mt-6">
+      <div className="row">
+        <div className="col col-3 col-lg-2 d-none d-md-block p-0">
+          <SideMenu />
+        </div>
+        <div className="col col-12 col-md-9 col-lg-10 p-0">
+          <div className="container-fluid" id="calendar-container">
+            <Route path="/app/day" component={DayView} />
+            <Route path="/app/week" component={WeekView} />
+            <Route path="/app/month" component={MonthView} />
+            <Route path="/app/year" component={YearView} />
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 const mapStateToProps = (state) => {
   return {
-    accessToken: null, // logged in user's current access token
-    isAuthenticated: false, // boolean indicating if a user is logged in
-    messages: null, // response messages
-    user: null, // object with auth user data
+    accessToken: state.accessToken, // logged in user's current access token
+    isAuthenticated: state.isAuthenticated, // boolean indicating if a user is logged in
+    messages: state.messages, // response messages
+    user: state.user, // object with auth user data
   };
 };
 
-export default connect(mapStateToProps)(Calendar)
+export default connect(mapStateToProps)(Calendar);
