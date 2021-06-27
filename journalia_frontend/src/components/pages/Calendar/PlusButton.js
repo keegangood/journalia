6b60a8b7ccd2b57a94847773511f680b2f1a0dbc
keegangood <keegood8@gmail.com>
2021-06-27 11:43:29 -0700

    adjusted styles

--- journalia_frontend/src/components/pages/Calendar/PlusButton.js
+++ journalia_frontend/src/components/pages/Calendar/PlusButton.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import {BsPlusCircleFill} from 'react-icons/bs';
+import {AiOutlinePlus} from 'react-icons/ai';
 import './scss/PlusButton.scss';
 
 const PlusButton = () => {
   return (
     <div className="w-auto position-fixed bottom-0 end-0 m-2 p-0" id="plus-button">
-      <BsPlusCircleFill/>
+      <AiOutlinePlus/>
     </div>
   )
 }
