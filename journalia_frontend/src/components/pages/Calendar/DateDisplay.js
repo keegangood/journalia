782c7bea0948f7ae1005348d965d2a16e80ad61b
unknown <keegood8@gmail.com>
2021-06-27 10:17:43 -0700

    created

--- journalia_frontend/src/components/pages/Calendar/DateDisplay.js
+++ journalia_frontend/src/components/pages/Calendar/DateDisplay.js
@@ -1,8 +1,24 @@
 import React from 'react'
 
+
+
 const DateDisplay = () => {
+  
+  const dayContent = (<span>
+    Jun 28 2021
+  </span>);
+  const weekContent = (<span>
+    Jun 28 - Jul 5, 2021
+  </span>);
+  const monthContent = (<span>
+    June, 2021
+  </span>);
+  const yearContent = (<span>
+    2021
+  </span>);
+
   return (
-    <div>
+    <div id="date-display">
       
     </div>
   )
