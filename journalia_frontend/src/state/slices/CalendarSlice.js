a31667e411353c8267166f8e996f97461d96d770
keegangood <keegood8@gmail.com>
2021-06-27 20:01:15 -0700

    moved CalendarSlice

---
+++ journalia_frontend/src/state/slices/CalendarSlice.js
@@ -0,0 +1,37 @@
+import dayjs from "dayjs";
+import { weekday } from "dayjs/plugin/weekday";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
+const BASE_URL = "http://localhost:8000/journals";
+const headers = {
+  "Content-Type": "application/json",
+};
+
+
+const initialState = {
+  journalItems: null,
+  calendarLoadingStatus: "PENDING",
+  date: null,
+  dayName: null
+};
+
+const CalendarSlice = createSlice({
+  name: "calendar",
+  initialState: initialState,
+  reducers: {
+    setDate: (state, action) => {
+      const date = action.payload;
+      let dayName = dayjs(date)
+      dayName = dayName.weekday(dayName.get('day')).format('dddd');
+      console.log('dayname', dayName)
+      return {
+        date: date,
+        dayName: dayName
+      };
+    },
+  },
+});
+
+export const { setDate } = CalendarSlice.actions;
+
+export default CalendarSlice.reducer;
