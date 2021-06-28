717df5efeffb365ee49c06124b02e991f0c8ea36
keegangood <keegood8@gmail.com>
2021-06-27 20:02:25 -0700

    pulled in props

--- journalia_frontend/src/components/pages/Calendar/DateDisplay.js
+++ journalia_frontend/src/components/pages/Calendar/DateDisplay.js
@@ -1,27 +1,22 @@
-import React from 'react'
+import React from "react";
 
+const DayContent = ({ date }) => (
+  <div className="col col-12 text-center">
+    <div>{date.dayName}</div>
+    <div>{date.date}</div>
+  </div>
+);
 
-
-const DateDisplay = () => {
-  
-  const dayContent = (<span>
-    Jun 28 2021
-  </span>);
-  const weekContent = (<span>
-    Jun 28 - Jul 5, 2021
-  </span>);
-  const monthContent = (<span>
-    June, 2021
-  </span>);
-  const yearContent = (<span>
-    2021
-  </span>);
+const DateDisplay = ({ date, dayName }) => {
+  const weekContent = <span>Jun 28 - Jul 5, 2021</span>;
+  const monthContent = <span>June, 2021</span>;
+  const yearContent = <span>2021</span>;
 
   return (
-    <div id="date-display">
-      
+    <div className="row mt-6" id="date-display">
+      {<DayContent date={date} dayName={dayName}/>}
     </div>
-  )
-}
+  );
+};
 
-export default DateDisplay
+export default DateDisplay;
