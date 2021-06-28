41f05899c17c5531f05f334c5eb95cc03b7c46db
keegangood <keegood8@gmail.com>
2021-06-27 20:01:31 -0700

    added calendar slice

--- journalia_frontend/src/state/slices/index.js
+++ journalia_frontend/src/state/slices/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import AuthSlice from "./auth/AuthSlice";
+import CalendarSlice from "./CalendarSlice";
 
 const rootReducer = combineReducers({
   auth: AuthSlice,
+  calendar: CalendarSlice
 });
 
 export default rootReducer;
