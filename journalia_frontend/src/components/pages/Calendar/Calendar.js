45f9cb3acdd29bca9975ba2e1c1316d9f45fad01
keegangood <keegood8@gmail.com>
2021-06-27 20:03:51 -0700

    prelim calendar logic

--- journalia_frontend/src/components/pages/Calendar/Calendar.js
+++ journalia_frontend/src/components/pages/Calendar/Calendar.js
@@ -2,10 +2,16 @@ import { React, useEffect } from "react";
 import { useDispatch, useSelector, connect } from "react-redux";
 import { Router, Route, useHistory } from "react-router-dom";
 
+import dayjs from "dayjs";
+import weekday from "dayjs/plugin/weekday";
+import customParseFormat from "dayjs/plugin/customParseFormat";
+import toObject from "dayjs/plugin/toObject";
+
 import "./scss/Calendar.scss";
 
 import SideMenu from "./SideMenu";
 import PlusButton from "./PlusButton";
+import DateDisplay from "./DateDisplay";
 
 import DayView from "./DayView/DayView";
 import WeekView from "./WeekView/WeekView";
@@ -13,9 +19,34 @@ import MonthView from "./MonthView/MonthView";
 import YearView from "./YearView/YearView";
 
 import { requestAccessToken } from "../../../state/slices/auth/AuthSlice";
+import { setDate } from "../../../state/slices/CalendarSlice";
+
+dayjs.extend(weekday);
+dayjs.extend(customParseFormat);
+dayjs.extend(toObject);
 
 const Calendar = ({ history }) => {
   const dispatch = useDispatch();
+
+  const { date, dayName } = useSelector((state) => state.calendar);
+
+  console.log("calendar date", date);
+
+  useEffect(() => {
+    // const dayOfWeek = dayjs().get('day')
+    let date = dayjs()
+    let time = date.format()
+    date = date.format('MMMM D, YYYY')
+    console.log('date',date)
+    
+    // console.log('day', date.weekday(date.get('day')).format('dd'))
+    // const dayName = date.weekday(date.get('day')).toObject();
+
+
+    // console.log('dayName', date)
+    // console.log('day', dayOfWeek)
+    dispatch(setDate(date));
+  }, []);
 
   return (
     <div className="container-fluid mt-6 position-relative">
@@ -23,15 +54,16 @@ const Calendar = ({ history }) => {
         <div className="col col-3 col-lg-2 d-none d-md-block p-0">
           <SideMenu />
         </div>
-        <div className="col col-12 col-md-9 col-lg-10 p-0">
-          <div className="container-fluid" id="calendar-container">
+        <div className="col col-12 col-md-9 col-lg-10 p-0 mt-6">
+          <div className="container-fluid  mt-6 pt-6" id="calendar-container">
+            <DateDisplay date={date} dayName={dayName} />
             <Route path="/app/day" component={DayView} />
             <Route path="/app/week" component={WeekView} />
             <Route path="/app/month" component={MonthView} />
             <Route path="/app/year" component={YearView} />
           </div>
         </div>
-        <PlusButton/>
+        <PlusButton />
       </div>
     </div>
   );
@@ -39,10 +71,12 @@ const Calendar = ({ history }) => {
 
 const mapStateToProps = (state) => {
   return {
-    accessToken: state.accessToken, // logged in user's current access token
-    isAuthenticated: state.isAuthenticated, // boolean indicating if a user is logged in
-    messages: state.messages, // response messages
-    user: state.user, // object with auth user data
+    accessToken: state.auth.accessToken, // logged in user's current access token
+    isAuthenticated: state.auth.isAuthenticated, // boolean indicating if a user is logged in
+    messages: state.auth.messages, // response messages
+    user: state.auth.user, // object with auth user data
+    date: state.calendar.date,
+    dayName: state.calendar.dayName
   };
 };
 
