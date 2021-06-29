a67cd2316fda5e65bf694c22709cd2b3bc7e962a
unknown <keegood8@gmail.com>
2021-06-28 23:31:58 -0700

    set position fixed

--- journalia_frontend/src/components/pages/Calendar/DateDisplay.js
+++ journalia_frontend/src/components/pages/Calendar/DateDisplay.js
@@ -1,9 +1,11 @@
 import React from "react";
 
-const DayContent = ({ date }) => (
-  <div className="col col-12 text-center">
-    <div>{date.dayName}</div>
-    <div>{date.date}</div>
+import "./scss/DateDisplay.scss";
+
+const DayContent = ({ date, dayName }) => (
+  <div className="py-2 text-center flex flex-col align-items-center justify-content-center">
+    <div className="display-6">{dayName}</div>
+    <div>{date}</div>
   </div>
 );
 
@@ -13,8 +15,8 @@ const DateDisplay = ({ date, dayName }) => {
   const yearContent = <span>2021</span>;
 
   return (
-    <div className="row mt-6" id="date-display">
-      {<DayContent date={date} dayName={dayName}/>}
+    <div className="px-0 position-fixed w-100" id="date-display">
+        <DayContent date={date} dayName={dayName} />
     </div>
   );
 };
