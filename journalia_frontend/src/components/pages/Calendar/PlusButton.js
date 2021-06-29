27d3fa3c6e04c46e55dc8187f3954011fbf842b6
unknown <keegood8@gmail.com>
2021-06-28 23:32:27 -0700

    fixed zindex

--- journalia_frontend/src/components/pages/Calendar/PlusButton.js
+++ journalia_frontend/src/components/pages/Calendar/PlusButton.js
@@ -4,7 +4,7 @@ import './scss/PlusButton.scss';
 
 const PlusButton = () => {
   return (
-    <div className="w-auto position-fixed bottom-0 end-0 m-2 p-0" id="plus-button">
+    <div className="w-auto position-fixed m-2 p-0" id="plus-button">
       <AiOutlinePlus/>
     </div>
   )
