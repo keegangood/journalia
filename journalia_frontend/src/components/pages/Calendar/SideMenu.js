9c43cd6a50e2759422da48062eae0f319b10a9a7
unknown <keegood8@gmail.com>
2021-06-28 23:27:06 -0700

    simplidfied component and removed stickiness

--- journalia_frontend/src/components/pages/Calendar/SideMenu.js
+++ journalia_frontend/src/components/pages/Calendar/SideMenu.js
@@ -4,15 +4,13 @@ import "./scss/SideMenu.scss";
 
 const SideMenu = () => {
   return (
-    <div id="side-menu" className="position-sticky sticky-top">
-      <div className="p-3">
-        Create New
-        <ul className="decoration-none">
-          <li>Note</li>
-          <li>Event</li>
-          <li>Task</li>
-        </ul>
-      </div>
+    <div id="side-menu" className="position-fixed px-4">
+      Create New
+      <ul className="decoration-none">
+        <li>Note</li>
+        <li>Event</li>
+        <li>Task</li>
+      </ul>
     </div>
   );
 };
