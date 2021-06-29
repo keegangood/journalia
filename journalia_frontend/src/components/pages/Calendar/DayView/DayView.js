75e156f7bfdc005ae5e7de8ed5c354d52676d9d0
unknown <keegood8@gmail.com>
2021-06-28 23:32:56 -0700

    added lorem nonsense

--- journalia_frontend/src/components/pages/Calendar/DayView/DayView.js
+++ journalia_frontend/src/components/pages/Calendar/DayView/DayView.js
@@ -1,5 +1,5 @@
 import { React, useEffect } from "react";
-import '../scss/DayView.scss';
+import "../scss/DayView.scss";
 
 import { useDispatch } from "react-redux";
 
@@ -7,9 +7,122 @@ const DayView = () => {
   const dispatch = useDispatch();
 
   return (
-      <div className="row" id="day-view">
-        <div className="col col-12 text-center">DAY</div>
+    <div className="container-fluid" id="day-view">
+      <div className="row">
+        <span className="my-5"></span>
+        <div className="col col-12 px-0 text-center">
+          DAY
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto,
+            placeat consequatur? Quibusdam facilis, placeat dolores ex minus
+            commodi inventore ducimus, ipsa error voluptatem officiis quia, non
+            deleniti sunt voluptatibus aliquam?
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto,
+            placeat consequatur? Quibusdam facilis, placeat dolores ex minus
+            commodi inventore ducimus, ipsa error voluptatem officiis quia, non
+            deleniti sunt voluptatibus aliquam?
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto,
+            placeat consequatur? Quibusdam facilis, placeat dolores ex minus
+            commodi inventore ducimus, ipsa error voluptatem officiis quia, non
+            deleniti sunt voluptatibus aliquam?
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto,
+            placeat consequatur? Quibusdam facilis, placeat dolores ex minus
+            commodi inventore ducimus, ipsa error voluptatem officiis quia, non
+            deleniti sunt voluptatibus aliquam?
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto,
+            placeat consequatur? Quibusdam facilis, placeat dolores ex minus
+            commodi inventore ducimus, ipsa error voluptatem officiis quia, non
+            deleniti sunt voluptatibus aliquam?
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto,
+            placeat consequatur? Quibusdam facilis, placeat dolores ex minus
+            commodi inventore ducimus, ipsa error voluptatem officiis quia, non
+            deleniti sunt voluptatibus aliquam?
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto,
+            placeat consequatur? Quibusdam facilis, placeat dolores ex minus
+            commodi inventore ducimus, ipsa error voluptatem officiis quia, non
+            deleniti sunt voluptatibus aliquam?
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto,
+            placeat consequatur? Quibusdam facilis, placeat dolores ex minus
+            commodi inventore ducimus, ipsa error voluptatem officiis quia, non
+            deleniti sunt voluptatibus aliquam?
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto,
+            placeat consequatur? Quibusdam facilis, placeat dolores ex minus
+            commodi inventore ducimus, ipsa error voluptatem officiis quia, non
+            deleniti sunt voluptatibus aliquam?
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto,
+            placeat consequatur? Quibusdam facilis, placeat dolores ex minus
+            commodi inventore ducimus, ipsa error voluptatem officiis quia, non
+            deleniti sunt voluptatibus aliquam?
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto,
+            placeat consequatur? Quibusdam facilis, placeat dolores ex minus
+            commodi inventore ducimus, ipsa error voluptatem officiis quia, non
+            deleniti sunt voluptatibus aliquam?
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto,
+            placeat consequatur? Quibusdam facilis, placeat dolores ex minus
+            commodi inventore ducimus, ipsa error voluptatem officiis quia, non
+            deleniti sunt voluptatibus aliquam?
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto,
+            placeat consequatur? Quibusdam facilis, placeat dolores ex minus
+            commodi inventore ducimus, ipsa error voluptatem officiis quia, non
+            deleniti sunt voluptatibus aliquam?
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto,
+            placeat consequatur? Quibusdam facilis, placeat dolores ex minus
+            commodi inventore ducimus, ipsa error voluptatem officiis quia, non
+            deleniti sunt voluptatibus aliquam?
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto,
+            placeat consequatur? Quibusdam facilis, placeat dolores ex minus
+            commodi inventore ducimus, ipsa error voluptatem officiis quia, non
+            deleniti sunt voluptatibus aliquam?
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto,
+            placeat consequatur? Quibusdam facilis, placeat dolores ex minus
+            commodi inventore ducimus, ipsa error voluptatem officiis quia, non
+            deleniti sunt voluptatibus aliquam?
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto,
+            placeat consequatur? Quibusdam facilis, placeat dolores ex minus
+            commodi inventore ducimus, ipsa error voluptatem officiis quia, non
+            deleniti sunt voluptatibus aliquam?
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto,
+            placeat consequatur? Quibusdam facilis, placeat dolores ex minus
+            commodi inventore ducimus, ipsa error voluptatem officiis quia, non
+            deleniti sunt voluptatibus aliquam?
+          </p>
+        </div>
       </div>
+    </div>
   );
 };
 
