577d1d8642986da27335bd8e8753be916773420f
unknown <keegood8@gmail.com>
2021-07-18 20:41:20 -0700

    added timezone field

--- journalia_backend/users/admin.py
+++ journalia_backend/users/admin.py
@@ -3,13 +3,17 @@ from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
 from django.utils.translation import ugettext_lazy as _
 from .models import User, RefreshToken
 
+
 @admin.register(User)
 class UserAdmin(DjangoUserAdmin):
     """Define admin model for custom User model with no email field."""
 
+    # exclude=('password',)
+
     fieldsets = (
-        (None, {'fields': ('email', 'username', 'password')}),
-        (_('Personal info'), {'fields': ('first_name', 'last_name')}),
+        (None, {'fields': ('email', 'username')}),
+        (_('Personal info'), {
+         'fields': ('first_name', 'last_name', 'timezone')}),
         (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
                                        'groups', 'user_permissions')}),
         (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
@@ -23,5 +27,12 @@ class UserAdmin(DjangoUserAdmin):
     list_display = ('email', 'username', 'first_name', 'last_name', 'is_staff')
     search_fields = ('email', 'username', 'first_name', 'last_name')
     ordering = ('email',)
+
+    def formfield_for_choice_field(self, db_field, request, **kwargs):
+        if db_field == 'timezone':
+            kwargs['choices'] = timezones
+
+        return super().formfield_for_choice_field(db_field, request, **kwargs)
+
 
 admin.site.register(RefreshToken)
