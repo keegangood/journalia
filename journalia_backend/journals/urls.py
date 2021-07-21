535ce716255224382cedfce1bedbe7ad2655f0cf
keegangood <keegood8@gmail.com>
2021-07-20 21:51:27 -0700

    added date_interval parameter

--- journalia_backend/journals/urls.py
+++ journalia_backend/journals/urls.py
@@ -6,7 +6,7 @@ from . import views
 
 urlpatterns = [
     path('', views.journal_item_list),
-    path('<str:start_date>&<str:end_date>/', views.journal_item_list),
+    path('<str:start_date>&<str:end_date>&<str:date_interval>/', views.journal_item_list),
     path('detail/', views.journal_item_detail),
     path('update/', views.journal_item_detail),
     path('delete/', views.journal_item_delete)
